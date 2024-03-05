9970a11212b2b4cd79ca313be40d3d991714cbd4
Nafsikala <nafsikala2@gmail.com>
2024-03-05 19:40:31 +0100

    djxzckf

--- Ass 3/color_clustering.py
+++ Ass 3/color_clustering.py
@@ -9,10 +9,12 @@ import assignment as ass
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 
-def color_model(visible_voxels_colors_per_cam, visible_voxels_per_cam):
-    '''
+
+
+'''def color_model():
+    
     Creates the color models for all the subjects for all the cameras
-    '''
+    
 
     # NOTE: visible_voxels_per_cam, visible_voxels_colors_per_cam
     # NOTE: change the code to other names and shape it differently
@@ -58,7 +60,7 @@ def color_model(visible_voxels_colors_per_cam, visible_voxels_per_cam):
 
         cam_color_models[n_camera] = color_models
 
-    return cam_color_models # color_models is a list of n_camera lists of n_people color models
+    return cam_color_models # color_models is a list of n_camera lists of n_people color models'''
 
 '''def match_center(centers, centers_no_outliers):
     
@@ -76,33 +78,34 @@ def color_model(visible_voxels_colors_per_cam, visible_voxels_per_cam):
 
     return centers_no_outliers_matched'''
 
-def remove_outliers_and_ghosts(labels_def, centers, voxels):
+def remove_outliers_and_ghosts(labels_def, centers, voxels, voxels_no_height):
     '''
     Remove the outliers and the ghost voxels to improve the clustering
     '''
     distance = []
-    for i in range(len(voxels)):
+    for i in range(len(voxels_no_height)):
         center = centers[labels_def[i]]
-        distance.append(np.linalg.norm(voxels[i] - center))
+        distance.append(np.linalg.norm(voxels_no_height[i] - center))
 
     # Calculate the 0.05 percentile of the distances
-    threshold = np.percentile(distance, 95)
+    threshold = np.percentile(distance, 90)
 
     no_outliers_mask = distance < threshold
-    voxels_no_outliers = voxels[no_outliers_mask]
+    voxels_no_outliers = np.float32(voxels)[no_outliers_mask]
+    voxels_no_outliers_no_height = voxels_no_height[no_outliers_mask]
     # labels_def_no_outliers = labels_def[no_outliers_mask]
 
     # Remove the ghost voxels
     '''    for i in range(len(voxels_no_outliers)):
-              for j in range(i+1, len(voxels_no_outliers)):
-                  if labels_def_no_outliers[i] == labels_def_no_outliers[j]:
-                    if np.linalg.norm(voxels_no_outliers[i] - voxels_no_outliers[j]) < 10:
-                      labels_def_no_outliers[j] = -1'''
+        for j in range(i+1, len(voxels_no_outliers)):
+            if labels_def_no_outliers[i] == labels_def_no_outliers[j]:
+                if np.linalg.norm(voxels_no_outliers[i] - voxels_no_outliers[j]) < 10:
+                    labels_def_no_outliers[j] = -1'''
 
     # Cluster voxels in 3d space based on x/y information
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 0.1)
 
-    _, labels_def_no_outliers, centers_no_outliers = cv.kmeans(voxels_no_outliers, 4, None, criteria, 20, cv.KMEANS_RANDOM_CENTERS)
+    _, labels_def_no_outliers, centers_no_outliers = cv.kmeans(voxels_no_outliers_no_height, 4, None, criteria, 20, cv.KMEANS_RANDOM_CENTERS)
 
     # centers_no_outliers_matched = match_center(centers, centers_no_outliers)
 
@@ -114,12 +117,12 @@ def cluster_voxels(voxels):
     '''
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 0.1) # number of iterations, epsilon value (either when epsilon or number of iteration is reached)
     
-    voxels = np.float32(voxels)[:, [0, 2]] # Remove first and third column
+    voxels_no_height = np.float32(voxels)[:, [0, 2]] # Keep first and third column
 
-    _, labels_def, centers = cv.kmeans(voxels, 4, None, criteria, 20, cv.KMEANS_RANDOM_CENTERS) # 4 is number of clusters, 20 is number of attempts, centres randomly chosen
+    _, labels_def, centers = cv.kmeans(voxels_no_height, 4, None, criteria, 20, cv.KMEANS_RANDOM_CENTERS) # 4 is number of clusters, 20 is number of attempts, centres randomly chosen
 
     # TODO: IMPLEMENT REMOVAL OF OUTLIERS AND GHOSTS HERE
-    labels_def_no_outliers, centers_no_outliers, voxels_no_outliers = remove_outliers_and_ghosts(labels_def, centers, voxels)
+    labels_def_no_outliers, centers_no_outliers, voxels_no_outliers = remove_outliers_and_ghosts(labels_def, centers, voxels, voxels_no_height)
     # TODO: change the code a bit (variables, order and stuff)
 
     return labels_def_no_outliers, centers_no_outliers, voxels_no_outliers
@@ -134,7 +137,7 @@ def online_phase(cam_color_models, voxels, visible_voxels_colors_per_cam, visibl
     K-means clustering, a comparison of the offline color models to the online GMMs probabilities, a
     label matching to obtain the final labelling of each person and a 2D path tracking on the floor.
     """
-    # NOTE: Retrieve the correct parameters to give to the function
+    
     cam_color_models_offline = color_model(visible_voxels_colors_per_cam, visible_voxels_per_cam)
     cam_color_models_online = color_model(visible_voxels_colors_per_cam, visible_voxels_per_cam)
     
@@ -142,35 +145,33 @@ def online_phase(cam_color_models, voxels, visible_voxels_colors_per_cam, visibl
     log_likelihood_offline = []
     log_likelihood_online = []
 
-    for n_camera in range(1, settings.NUM_CAMERAS+1):
-        cost_matrix = np.zeros((len(voxels), len(cam_color_models_offline[n_camera-1])))
+    cost_matrix = []
+    for label_offline, color_model_offline in enumerate(cam_color_models_offline[n_camera]):
+        cost_row = []
+        for label_online, color_model_online in enumerate(cam_color_models_online[n_camera]):
+            
+            distance = calculate_distance(color_model_offline, color_model_online) # NOTE: fix function/implement it
+            cost_row.append(distance)
+            # PSEUDO:
+            # 1. Calculate the distance between the offline and the online model
+            # 2. Insert the distance in the cost matrix
+        cost_matrix = np.array(cost_matrix)
+    # 3. Call the hungarian algorithm
+    # 4. Save the values in the cost matrix
+    # Run the Hungarian algorithm
+    row_ind, col_ind = linear_sum_assignment(cost_matrix)
+    # The `row_ind` and `col_ind` arrays represent the optimal assignment
+    # You can use these indices to understand which offline model best matches with which online model
 
-        for label_offline, color_model_offline in enumerate(cam_color_models_offline[n_camera]):
-            # Get the GMM model for the person
-            for label_online, color_model_online in enumerate(cam_color_models_online[n_camera]):
-                # Get the probability of the person being in the scene
-                
-                # PSEUDO:
-                # 1. Calculate the distance between the offline and the online model
-                # 2. Insert the distance in the cost matrix
-                '''# Get the probability of the person being in the scene
-                log_likelihood_offline.append(model_offline.score_samples(roi)) # NOTE: not sure about roi
-                log_likelihood_online.append(model_online.score_samples(roi)) # NOTE: not sure about roi'''
-
-        # 3. Call the hungarian algorithm
-        # 4. Save the values in the cost matrix
+    # For handling ties or preferences over cameras, you would need to implement additional logic
+    # This could involve comparing distances or having predefined rules for tiebreakers
     
     # 5. Save based on majority label over the different cameras (hope for no ties or implement a preference system over the cameras)
 
-    # Calculate the cost matrix
-    cost_matrix[:, label] = log_likelihood_offline - log_likelihood_online
-
-    # Assign the labels to the people
-    row_ind, col_ind = linear_sum_assignment(cost_matrix)
     predictions.append(col_ind) # Store the optimal assignment for the current camera
 
     # Get the final labeling
-    final_labeling(cam_color_models_offline, visible_voxels_colors_per_cam, visible_voxels_per_cam, roi)
+    final_labeling(cam_color_models_offline, visible_voxels_colors_per_cam, visible_voxels_per_cam, roi) # to check and implement
 
     return predictions
 
